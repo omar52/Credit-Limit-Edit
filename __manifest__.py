bb902dd02ca479e3e657a0f885c0dd4086fefbd6
omarrabdoo00@gmail.com <omarrabdoo00@gmail.com>
2025-11-30 12:45:14 +0200

    Initial commit: Creating the module necessary Files & Directories

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Credit Limit Edit",
+    'author': "Omar Abdelkarim",
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base', 'sale', 'account', 'mail', 'contacts'],
+    'data': [  # path within the application
+
+    ],
+    'assets': {
+
+    },
+    'application': True,
+    'installable': True,
+}
