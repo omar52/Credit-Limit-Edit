eaacdf14328f21522825e9dc71f752be514c58be
omarrabdoo00@gmail.com <omarrabdoo00@gmail.com>
2025-11-30 13:07:23 +0200

    Creating the group whose members are allowed to edit credit_limit field

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'version': '17.0.0.1.0',
     'depends': ['base', 'sale', 'account', 'mail', 'contacts'],
     'data': [  # path within the application
-
+        'security/security.xml',
     ],
     'assets': {
 
